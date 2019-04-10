e9acc6d734cae2bf7c535f39b5e2fd4cf7444d58
EC2 Default User <ec2-user@ip-172-31-89-45.ec2.internal>
2019-04-10 20:14:17 +0000

    first run

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "612468997814-fspigmuf8jpk6l6tbpl6lm4hl3vf21qt.apps.googleusercontent.com", "7MIm74F-tl-6v-XRlNmvsyW2"
+  provider :google_oauth2, "292614196243-mmcsl3l5bktpqdhipd8sr89g64mipfir.apps.googleusercontent.com", "bsaU_c1ePWalyDxPEgvbXvc7"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,59 @@ ActiveRecord::Schema.define(version: 2019_04_04_004841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "articles_tags", id: false, force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "tag_id"
+  end
+
+  create_table "blogs", id: :serial, force: :cascade do |t|
+    t.text "settings"
+    t.string "base_url", limit: 255
+  end
+
+  create_table "categories", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
+    t.integer "position"
+    t.string "permalink", limit: 255
+    t.text "keywords"
+    t.text "description"
+    t.integer "parent_id"
+    t.index ["permalink"], name: "index_categories_on_permalink"
+  end
+
+  create_table "categorizations", id: :serial, force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.boolean "is_primary"
+  end
+
+  create_table "contents", id: :serial, force: :cascade do |t|
+    t.string "type", limit: 255
+    t.string "title", limit: 255
+    t.string "author", limit: 255
+    t.text "body"
+    t.text "extended"
+    t.text "excerpt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "user_id"
+    t.string "permalink", limit: 255
+    t.string "guid", limit: 255
+    t.integer "text_filter_id"
+    t.text "whiteboard"
+    t.string "name", limit: 255
+    t.boolean "published", default: false
+    t.boolean "allow_pings"
+    t.boolean "allow_comments"
+    t.datetime "published_at"
+    t.string "state", limit: 255
+    t.integer "parent_id"
+    t.text "settings"
+    t.string "post_type", limit: 255, default: "read"
+    t.index ["published"], name: "index_contents_on_published"
+    t.index ["text_filter_id"], name: "index_contents_on_text_filter_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -22,6 +75,102 @@ ActiveRecord::Schema.define(version: 2019_04_04_004841) do
     t.datetime "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "feedback", id: :serial, force: :cascade do |t|
+    t.string "type", limit: 255
+    t.string "title", limit: 255
+    t.string "author", limit: 255
+    t.text "body"
+    t.text "excerpt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "user_id"
+    t.string "guid", limit: 255
+    t.integer "text_filter_id"
+    t.text "whiteboard"
+    t.integer "article_id"
+    t.string "email", limit: 255
+    t.string "url", limit: 255
+    t.string "ip", limit: 40
+    t.string "blog_name", limit: 255
+    t.boolean "published", default: false
+    t.datetime "published_at"
+    t.string "state", limit: 255
+    t.boolean "status_confirmed"
+    t.index ["article_id"], name: "index_feedback_on_article_id"
+    t.index ["text_filter_id"], name: "index_feedback_on_text_filter_id"
+  end
+
+  create_table "notifications", id: :serial, force: :cascade do |t|
+    t.integer "content_id"
+    t.integer "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "page_caches", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
+    t.index ["name"], name: "index_page_caches_on_name"
+  end
+
+  create_table "pings", id: :serial, force: :cascade do |t|
+    t.integer "article_id"
+    t.string "url", limit: 255
+    t.datetime "created_at"
+    t.index ["article_id"], name: "index_pings_on_article_id"
+  end
+
+  create_table "post_types", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
+    t.string "permalink", limit: 255
+    t.string "description", limit: 255
+  end
+
+  create_table "profiles", id: :serial, force: :cascade do |t|
+    t.string "label", limit: 255
+    t.string "nicename", limit: 255
+    t.text "modules"
+  end
+
+  create_table "profiles_rights", id: false, force: :cascade do |t|
+    t.integer "profile_id"
+    t.integer "right_id"
+  end
+
+  create_table "redirections", id: :serial, force: :cascade do |t|
+    t.integer "content_id"
+    t.integer "redirect_id"
+  end
+
+  create_table "redirects", id: :serial, force: :cascade do |t|
+    t.string "from_path", limit: 255
+    t.string "to_path", limit: 255
+    t.string "origin", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "resources", id: :serial, force: :cascade do |t|
+    t.integer "size"
+    t.string "filename", limit: 255
+    t.string "mime", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "article_id"
+    t.boolean "itunes_metadata"
+    t.string "itunes_author", limit: 255
+    t.string "itunes_subtitle", limit: 255
+    t.integer "itunes_duration"
+    t.text "itunes_summary"
+    t.string "itunes_keywords", limit: 255
+    t.string "itunes_category", limit: 255
+    t.boolean "itunes_explicit"
+  end
+
+  create_table "rights", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
+    t.string "description", limit: 255
   end
 
   create_table "schedules", force: :cascade do |t|
@@ -34,6 +183,23 @@ ActiveRecord::Schema.define(version: 2019_04_04_004841) do
     t.index ["subject_id"], name: "index_schedules_on_subject_id"
   end
 
+  create_table "sidebars", id: :serial, force: :cascade do |t|
+    t.integer "active_position"
+    t.text "config"
+    t.integer "staged_position"
+    t.string "type", limit: 255
+  end
+
+  create_table "sitealizer", id: :serial, force: :cascade do |t|
+    t.string "path", limit: 255
+    t.string "ip", limit: 255
+    t.string "referer", limit: 255
+    t.string "language", limit: 255
+    t.string "user_agent", limit: 255
+    t.datetime "created_at"
+    t.date "created_on"
+  end
+
   create_table "subjects", force: :cascade do |t|
     t.string "name", null: false
     t.date "start_date"
@@ -41,6 +207,28 @@ ActiveRecord::Schema.define(version: 2019_04_04_004841) do
     t.integer "hours"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_subjects_on_user_id"
+  end
+
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "display_name", limit: 255
+  end
+
+  create_table "text_filters", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
+    t.string "description", limit: 255
+    t.string "markup", limit: 255
+    t.text "filters"
+    t.text "params"
+  end
+
+  create_table "triggers", id: :serial, force: :cascade do |t|
+    t.integer "pending_item_id"
+    t.string "pending_item_type", limit: 255
+    t.datetime "due_at"
+    t.string "trigger_method", limit: 255
   end
 
   create_table "users", force: :cascade do |t|
